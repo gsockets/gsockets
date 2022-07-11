526b3d44c193c23341165bb8703f5d08edcbeb54
Sayantan Das <das.sayantan94@gmail.com>
2022-07-11 15:25:28 +0530

    implemented config based app manager

---
+++ app_managers/app_manager.go
@@ -0,0 +1,21 @@
+package appmanagers
+
+import (
+	"errors"
+
+	"github.com/gsockets/gsockets"
+	"github.com/gsockets/gsockets/config"
+)
+
+var (
+	ErrInvalidAppManagerDriver = errors.New("invalid driver for app manager")
+)
+
+func New(appManagerConfig config.AppManager) (gsockets.AppManager, error) {
+	switch appManagerConfig.Driver {
+	case "array":
+		return newConfigAppManager(appManagerConfig.Array), nil
+	default:
+		return nil, nil
+	}
+}
---
+++ app_managers/config_manager.go
@@ -0,0 +1,55 @@
+package appmanagers
+
+import (
+	"context"
+
+	"github.com/gsockets/gsockets"
+)
+
+type configAppManager struct {
+	apps map[string]*gsockets.App
+}
+
+func newConfigAppManager(appsConfig []gsockets.App) gsockets.AppManager {
+	apps := make(map[string]*gsockets.App)
+	for _, app := range appsConfig {
+		apps[app.ID] = &app
+	}
+
+	return &configAppManager{apps: apps}
+}
+
+// FindById returns an app instance by the app id.
+func (config *configAppManager) FindById(ctx context.Context, id string) (*gsockets.App, error) {
+	app, ok := config.apps[id]
+	if !ok {
+		return nil, nil
+	}
+
+	return app, nil
+}
+
+// FindByKey returns an app instance by app key.
+func (config *configAppManager) FindByKey(ctx context.Context, key string) (*gsockets.App, error) {
+	for _, app := range config.apps {
+		if app.Key == key {
+			return app, nil
+		}
+	}
+
+	return nil, nil
+}
+
+// FindBySecret returns an app instance by app secret.
+func (config *configAppManager) GetAppSecret(ctx context.Context, id string) (string, error) {
+	app, err := config.FindById(ctx, id)
+	if err != nil {
+		return "", err
+	}
+
+	if app == nil {
+		return "", nil
+	}
+
+	return app.Secret, nil
+}
